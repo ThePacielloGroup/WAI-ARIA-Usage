814cb70f16a449d7c5fe1b5c0b274b4e6a198f80
Scott O'Hara <scottaohara@users.noreply.github.com>
2021-11-22 12:16:24 -0500

    single return false update

--- aria-usage.js
+++ aria-usage.js
@@ -2099,12 +2099,11 @@ function checkValidProperties(objElement, strRole) {
 			if (strType === "checkbox") {
 				if ( objElement.getAttribute("aria-checked") === "mixed" ) {
 					logResult("Error: ", strTagName, " aria-checked=mixed is used on a native checkbox. Use the element's indeterminate IDL attribute instead.", "", objElement, ".", "invalidproperty");
-					return false;
 				}
 				else {
-					logResult("Error: ", strTagName, " aria-checked is used on a native checkbox. Use the element's native checked semantics instead.", "", objElement, ".", "invalidproperty");
-					return false;
+					logResult("Error: ", strTagName, " aria-checked is used on a native checkbox. Use the element's native checked semantics instead.", "", objElement, ".", "invalidproperty");	
 				}
+				return false;
 			}
 			else if (strType === "radio") {
 				logResult("Error: ", strTagName, " aria-checked is used on a native radio button. Use the element's native checked semantics instead.", "", objElement, ".", "invalidproperty");
